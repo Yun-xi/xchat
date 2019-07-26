ecd6122c960c64261b342601b4fe283c4a8c81c3
xieyaqi <987159036@qq.com>
2019-07-26 17:55:37 +0800

    新增xchat-common模块

---
+++ xchat-common/src/main/java/com/xx/xchat/XchatCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xx.xchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchatCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XchatCommonApplication.class, args);
+    }
+
+}
