74505c261e84a92ce48d78010ba30ec47b171e93
XieYaqi <987159036@qq.com>
2019-11-25 22:56:57 +0800

    add api

---
+++ xchat-api/src/main/java/com/xxxx/xchat/XchatApiApplication.java
@@ -0,0 +1,13 @@
+package com.xxxx.xchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XchatApiApplication.class, args);
+    }
+
+}
---
+++ xchat-api/src/test/java/com/xxxx/xchat/XchatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxx.xchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
