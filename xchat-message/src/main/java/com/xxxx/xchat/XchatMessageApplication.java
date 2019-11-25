d7e3ef57c5f2577b4b51a1f0fa87c45ff3671e29
xieyaqi <987159036@qq.com>
2019-11-25 14:48:16 +0800

    新增message模块

---
+++ xchat-message/src/main/java/com/xxxx/xchat/XchatMessageApplication.java
@@ -0,0 +1,13 @@
+package com.xxxx.xchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchatMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XchatMessageApplication.class, args);
+    }
+
+}
---
+++ xchat-message/src/test/java/com/xxxx/xchat/XchatMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxx.xchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchatMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
